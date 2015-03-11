c24bd16108f96cfb9282b7579ee53c4a10818954
frankmenesesb <frankmeneses10@gmail.com>
2015-03-11 18:07:44 -0500

    se esta creando lista
    lista del proveedor

---
+++ js/get/getProveedor.js
@@ -0,0 +1,104 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var articulos = new Array();
+
+$(function () {
+    //$(".find_button").click(function () {
+
+
+    //variables de entrada
+    //var strIde = $("#txtIdentificacion").val();
+
+    //variables locales
+
+    var id;
+    var descripcion;
+    var html;
+
+    //var dataString = {'identificacion': strIde};
+
+
+    $.ajax({
+        type: 'POST',
+        //data: dataString,
+        dataType: 'json',
+        url: "http://refinal.frienderco.com/php/get/getArticulos.php",
+        //url: "../php/get/getArticulos.php",
+        success: function (jsonResp) {
+
+            if (jsonResp.RESPONSE) {
+
+
+                if (jsonResp.MESSAGE === "undefined" || jsonResp.MESSAGE === undefined) {
+
+                    alert('Error no hay articulos!!');
+                }
+                if (jsonResp.MESSAGE === "") {
+
+
+
+                    for (var i = 0; i < jsonResp.DATA.length; i++) {
+
+
+
+
+                        id = jsonResp.DATA[i]["id_art"];
+                        descripcion = jsonResp.DATA[i]["descripcion"];
+
+
+
+                        var log = "";
+                        if ((descripcion === null || descripcion === "") || (id === null || id === "")) {
+
+                            alert("Error: articulos con errores o sin existencia ");
+
+                        } else {
+
+
+
+                            html += '<tr>';
+                            html += '<td>';
+                            html += ''+descripcion+''
+                            html += '</td>';
+                            html += '<td>';
+                            html += '<input type="number" class="form-control" id="'+id+'" placeholder="0 Kg."/>';
+                            html += '</td>';
+                            html += '<td>';
+                            html += '<label>Kg.</label>';
+                            html += '</td>';
+                            html += '</tr>';
+                            //articulos.add(id);
+
+
+
+
+
+
+                        }
+                    }
+                    $("#recibo").html(html);
+                    //$("#txtHint").html(encabezado+html+final);
+
+                } else if (jsonResp.MESSAGE === "EMPTY") {
+                    alert("Error: no se encontro datos de articulos!!");
+                }
+            } else {
+                alert("Ocurrio Un error:" + jsonResp.MESSAGE);
+            }
+
+
+        },
+        error: function (jsonResp) {
+            alert("Ocurrio Un error");
+        }
+    });
+
+
+    // });
+
+});
+
---
+++ php/get/getProveedor.php
@@ -0,0 +1,59 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    include("../config.php");
+
+    $blResp = false;
+
+    $strMessage = "";
+
+    //$q = intval($_REQUEST['strUser']);
+    
+    //$login = htmlspecialchars(trim($_REQUEST['usuario']));
+    //$pass1 = sha1(md5(trim($_REQUEST['contrasena'])));
+    
+    //variable donde traigo la identificacion
+    //$identificacion = htmlspecialchars(trim($_REQUEST['identificacion']));
+    
+    
+    $con = mysqli_connect($datos[0],$datos[1],$datos[2],$datos[3]);
+    $blResp = true;
+    
+    if (!$con) {
+        $blResp = false;
+        die('Could not connect: ' . mysqli_error($con));
+        $strMesage = "No fue posible conectarse: ".mysqli_error($con);
+    }
+
+    mysqli_select_db($con,"ajax_demo");
+
+
+$sql="select a.id_art, a.descripcion from articulos a order by a.id_art";
+//$sql="SELECT id, nombre, apellidos, login, foto FROM usuarios WHERE login= '$login' and password='$pass1'";
+$result = mysqli_query($con,$sql);
+
+$arrayData = array();
+
+while($row = mysqli_fetch_assoc($result)) {
+    array_push($arrayData,$row);
+    
+    
+}
+
+if(count($arrayData) === 0 || count($arrayData) === null || count($arrayData) === ''){
+    $strMessage = "EMPTY";
+}
+
+
+
+mysqli_close($con);
+
+$arrayResp = array(
+    'RESPONSE'  => $blResp,
+    'MESSAGE'   => $strMessage,
+    'DATA'      => $arrayData
+);
+
+echo json_encode($arrayResp);
+
+?>
