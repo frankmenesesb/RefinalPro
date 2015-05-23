2cb701a07c50c588e6c1b696fc35d473a484838c
frank <frankmeneses10@gmail.com>
2015-05-23 11:25:16 -0500

    cambio gestion de proveedores
    se creo lista de proveedores

--- js/get/getGProveedor.js
+++ js/get/getGProveedor.js
@@ -4,19 +4,85 @@
  * and open the template in the editor.
  */
 
-var articulos = new Array();
-
-$(function () {
-    //$(".find_button").click(function () {
 
 
-    //variables de entrada
-    //var strIde = $("#txtIdentificacion").val();
+$(document).ready(function () {
+    $('.filterable .btn-filter').click(function () {
+        var $panel = $(this).parents('.filterable'),
+                $filters = $panel.find('.filters input'),
+                $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') === true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
 
-    //variables locales
+    $('.filterable .filters input').keyup(function (e) {
+        /* Ignore tab key */
+        var code = e.keyCode || e.which;
+        if (code === '9')
+            return;
+        /* Useful DOM data and selectors */
+        var $input = $(this),
+                inputContent = $input.val().toLowerCase(),
+                $panel = $input.parents('.filterable'),
+                column = $panel.find('.filters th').index($input.parents('th')),
+                $table = $panel.find('.table'),
+                $rows = $table.find('tbody tr');
+        /* Dirtiest filter function ever ;) */
+        var $filteredRows = $rows.filter(function () {
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        /* Clean previous no-result if exist */
+        $table.find('tbody .no-result').remove();
+        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
+        $rows.show();
+        $filteredRows.hide();
+        /* Prepend no-result row if all rows are filtered */
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="' + $table.find('.filters th').length + '">No result found</td></tr>'));
+        }
+    });
+
+
+    allProveedores();
+    
+//    setSelectProveedores();
+//    
+//
+//
+//    $("#btnAddArticulo").on("click", function () {
+//        setArticulosInterveentor();
+//        
+//    });
+//
+//    $('#selProveedores').on('change', function () {
+//        getArticulosProveedor();
+//        
+//    });
+    
+    /*$('#btnNewProveedor').on('change', function () {
+        alert("x");
+       $('#dialogAddProveedor').modal('show');
+    });*/
+    
+    // 
+});
+
+
+
+function allProveedores() {
+    
 
     var id;
     var descripcion;
+    var rut;
+    var plaza;
     var html;
 
     //var dataString = {'identificacion': strIde};
@@ -43,6 +109,8 @@ $(function () {
 
                         id = jsonResp.DATA[i]["id_proveedor"];
                         descripcion = jsonResp.DATA[i]["nombre"];
+                        rut =jsonResp.DATA[i]["rut"];
+                        plaza = jsonResp.DATA[i]["nom_plaza"];
 
                         var log = "";
                         if ((descripcion === null || descripcion === "") || (id === null || id === "")) {
@@ -53,18 +121,34 @@ $(function () {
 
 
 
-                            html += '<option value="'+id+'">'+descripcion+'</option>';
-                            
-                            //articulos.add(id);
-
-
-
-
-
+                            html += '<tr id="row_' + i + '">';
+                        html += '<td style="width: 10%;">';
+                        html += '' + id + '';
+                        html += '</td>';
+                        html += '<td>';
+                        html += '' + descripcion + '';
+                        html += '</td>';
+                        html += '<td>';
+                        if(rut==='S'){
+                            html +='<img src="../images/icon activo.png" alt="">';
+                        }else{
+                            html +='<img src="../images/icon inactivo.png" alt="">';
+                        }
+                        html += '</td>';
+                        html += '<td>';
+                        html += '' + plaza + '';
+                        html += '</td>';
+                        html += '<td>';
+                        
+                        html += '</td>';
+                        html += '<td>';
+                        
+                        html += '</td>';
+                        html += '</tr>';
 
                         }
                     }
-                    $("#txtProveedor").html(html);
+                    $("#listProveedores tbody").html(html);
                     //$("#txtHint").html(encabezado+html+final);
 
                 } else if (jsonResp.MESSAGE === "EMPTY") {
@@ -84,5 +168,5 @@ $(function () {
 
     // });
 
-});
+};
 
